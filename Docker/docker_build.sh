30f8793ede70f96c45a00cbb4bce52ad7fb7d45c
Maksim <maksim.levental@gmail.com>
2017-02-27 14:50:37 -0500

    docker stuff

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -8,9 +8,5 @@ docker run -d --volumes-from databrary_dbstore -p 5432:5432 --rm --name databrar
 ./wait-for-postgres.sh localhost "docker exec databrary_postgres /usr/local/src/databrary/init-user-db.sh"
 docker build -t databrary_databrary:config -f databrary/Dockerfile_config ./databrary
 
-#docker run --rm --link databrary_postgres:databrary_postgres -w /home/databrary/src/Schemabrary databrary_databrary:config stack exec schemabrary -- -c ../databrary.conf
-
-#docker run --link databrary_postgres:databrary_postgres --name databrary_config databrary_databrary:config stack build
-#docker commit -m "build databary" databrary_config databrary_databrary:final
 docker build -t databrary_databrary:final -f databrary/Dockerfile_final ./databrary
 docker stop databrary_postgres
