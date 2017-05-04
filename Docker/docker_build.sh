9dda10f86958fafea8a70dc46cef0e2e2fac210c
makslevental <maksim.levental@gmail.com>
2017-05-04 15:16:01 -0400

    add docker readme

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -6,8 +6,8 @@
 
 docker volume create --name databrary_postgres_store
 docker build -t databrary_postgres postgres/
-docker run -d -v databrary_postgres_store:/var/lib/postgresql/data -p 5433:5433 --rm --name databrary_postgres databrary_postgres
+docker run -d -v databrary_postgres_store:/var/lib/postgresql/data -p 5432:5432 --rm --name databrary_postgres databrary_postgres
 ./wait-for-postgres.sh localhost "docker exec databrary_postgres /usr/local/src/databrary/init-user-db.sh"
 
 ./wait-for-postgres.sh localhost "docker exec databrary_postgres /usr/local/src/databrary/remove-superuser-db.sh"
-docker stop databrary_postgres
+# docker stop databrary_postgres
