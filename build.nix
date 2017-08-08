34c7e796bdaeb6e19f0bb796c83a19c2c3177fae
hsloan <ishaqsloan@gmail.com>
2017-08-08 18:01:16 -0400

    build.nix with postgresql and invertible test suite overrides

--- build.nix
+++ build.nix
@@ -1,4 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-nixpkgs.haskellPackages.callPackage ./. {
-  inherit (nixpkgs) ffmpeg;
-}
+{ reflex-platform ? import ./reflex-platform {} }:
+
+let 
+	nixpkgs = reflex-platform.nixpkgs;
+  dontCheck = nixpkgs.haskell.lib.dontCheck;
+  pkgs = reflex-platform.ghc.override {
+    overrides = self: super: {
+      databrary = self.callPackage ./. {inherit (nixpkgs) ffmpeg;};
+    	postgresql-typed = dontCheck super.postgresql-typed; 
+			invertible = dontCheck super.invertible;
+    };
+  };
+in pkgs.databrary
