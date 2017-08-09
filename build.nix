623ce822383ac1a36f31bee92bb76c76bc7ac8aa
hsloan <ishaqsloan@gmail.com>
2017-08-09 17:48:41 -0400

    added  ghc7 support to get around outdated TH errors

--- build.nix
+++ build.nix
@@ -3,11 +3,26 @@
 let 
 	nixpkgs = reflex-platform.nixpkgs;
   dontCheck = nixpkgs.haskell.lib.dontCheck;
-  pkgs = reflex-platform.ghc.override {
+
+  # ghc 8 fails with template haskell errors
+  #pkgs = reflex-platform.ghc.override {
+
+  # reflex-platform.ghc7 fails with infinite recursion
+  # 
+	pkgs =	nixpkgs.pkgs.haskell.packages.ghc7103.override {
     overrides = self: super: {
       databrary = self.callPackage ./. {inherit (nixpkgs) ffmpeg;};
-    	postgresql-typed = dontCheck super.postgresql-typed; 
+      # nix version is 0.5.1
+    	# postgresql-typed = dontCheck super.postgresql-typed; 
+      # databrary is using 0.4.5
+    	postgresql-typed = dontCheck (self.callHackage  "postgresql-typed"
+      "0.4.5" {});
+			
+			#partial-isomorphisms is for GHC7 only!
+			partial-isomorphisms= dontCheck (self.callHackage  "partial-isomorphisms"
+      "0.2" {});
+
 			invertible = dontCheck super.invertible;
     };
   };
-in pkgs.databrary
+in { inherit nixpkgs pkgs; } 
