3fee3dc358dbcbf26a61200aecba2f490d734521
hsloan <ishaqsloan@gmail.com>
2017-08-10 16:32:11 -0400

    update Template Haskell type sigs

--- Databrary/Model/Id/Types.hs
+++ Databrary/Model/Id/Types.hs
@@ -9,7 +9,7 @@ import Control.Arrow (first)
 import qualified Data.Aeson as JSON
 import Data.Hashable (Hashable(..))
 import Data.Int (Int32)
-import Database.PostgreSQL.Typed.Types (PGParameter(..), PGColumn(..))
+import Database.PostgreSQL.Typed.Types (PGParameter(..), PGColumn(..), PGType(..))
 import Database.PostgreSQL.Typed.Dynamic (PGRep)
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as TH
@@ -28,14 +28,14 @@ instance Hashable (IdType a) => Hashable (Id a) where
   hashWithSalt i = hashWithSalt i . unId
   hash = hash . unId
 
-instance PGParameter t (IdType a) => PGParameter t (Id a) where
+instance (PGParameter t (IdType a), PGType t) => PGParameter t (Id a) where
   pgEncode t (Id i) = pgEncode t i
   pgEncodeValue e t (Id i) = pgEncodeValue e t i
   pgLiteral t (Id i) = pgLiteral t i
-instance PGColumn t (IdType a) => PGColumn t (Id a) where
+instance (PGColumn t (IdType a), PGType t) => PGColumn t (Id a) where
   pgDecode t = Id . pgDecode t
   pgDecodeValue e t = Id . pgDecodeValue e t
-instance (PGParameter t (IdType a), PGColumn t (IdType a), PGRep t (IdType a)) => PGRep t (Id a)
+instance (PGParameter t (IdType a), PGColumn t (IdType a), PGRep t (IdType a), PGType t) => PGRep t (Id a)
 
 instance Show (IdType a) => Show (Id a) where
   showsPrec p (Id a) = showsPrec p a
