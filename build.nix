0f1f85f04deab2f49451b9e2a0aeab36a191fa0e
hsloan <ishaqsloan@gmail.com>
2017-08-10 16:43:29 -0400

    hjsonschema ghc overrides added, along with other expiramental procedures

--- build.nix
+++ build.nix
@@ -1,28 +1,38 @@
-{ reflex-platform ? import ./reflex-platform {} }:
+{ reflex-platform ? import ./reflex-platform {} 
+  #compiler ? ""
+}:
 
 let 
 	nixpkgs = reflex-platform.nixpkgs;
   dontCheck = nixpkgs.haskell.lib.dontCheck;
+  overrideCabal = nixpkgs.haskell.lib.overrideCabal;
+	doJailbreak = nixpkgs.haskell.lib.doJailbreak;
 
   # ghc 8 fails with template haskell errors
-  #pkgs = reflex-platform.ghc.override {
+  pkgs = reflex-platform.ghc.override {
 
   # reflex-platform.ghc7 fails with infinite recursion
   # 
-	pkgs =	nixpkgs.pkgs.haskell.packages.ghc7103.override {
-    overrides = self: super: {
+	# pkgs =	nixpkgs.pkgs.haskell.packages.ghc7103.override {
+    overrides = self: super: rec {
       databrary = self.callPackage ./. {inherit (nixpkgs) ffmpeg;};
+      
+      databrary-dev = overrideCabal databrary (drv: {
+        libraryHaskellDepends = (drv.libraryHaskellDepends or []) ++ [self.ghcid];
+      });
+
       # nix version is 0.5.1
     	# postgresql-typed = dontCheck super.postgresql-typed; 
       # databrary is using 0.4.5
-    	postgresql-typed = dontCheck (self.callHackage  "postgresql-typed"
-      "0.4.5" {});
+    	postgresql-typed = dontCheck (self.callHackage  "postgresql-typed" "0.4.5" {});
 			
 			#partial-isomorphisms is for GHC7 only!
-			partial-isomorphisms= dontCheck (self.callHackage  "partial-isomorphisms"
-      "0.2" {});
+			# partial-isomorphisms= dontCheck (self.callHackage  "partial-isomorphisms"
+      #"0.2" {});
 
-			invertible = dontCheck super.invertible;
+			hjsonschema = dontCheck (doJailbreak (self.callHackage "hjsonschema" "0.9.0.0" {}));	
+			hjsonpointer = dontCheck (doJailbreak (self.callHackage "hjsonpointer" "0.3.0.2" {}));
+		 	invertible = dontCheck super.invertible;
     };
   };
 in { inherit nixpkgs pkgs; } 
