7d70925ba96e572cd9f2393f9ce55ae8368a8a50
hsloan <ishaqsloan@gmail.com>
2017-08-17 20:03:39 -0400

    force call stylus from node_modules (dirty haq)

--- Databrary/Web/Stylus.hs
+++ Databrary/Web/Stylus.hs
@@ -13,11 +13,18 @@ import Databrary.Web
 import Databrary.Web.Types
 import Databrary.Web.Files
 import Databrary.Web.Generate
+import Data.Monoid
 
 generateStylusCSS :: WebGenerator
 generateStylusCSS fo@(f, _) = do
   let src = "app.styl"
+  -- liftIO $ print $ webFileAbs f <> " " <> webFileAbs src
   sl <- liftIO $ findWebFiles ".styl"
   webRegenerate
-    (callProcess (binDir </> "stylus") $ (if takeExtensions (webFileRel f) == ".min.css" then ("-c":) else id) ["-u", "nib", "-u", "autoprefixer-stylus", "-o", webFileAbs f, webFileAbs src])
-    [] sl fo
+    (callProcess
+      "./node_modules/.bin/stylus" $
+    (if takeExtensions (webFileRel f) == ".min.css" then ("-c":) else id) 
+    ["-u", "nib", "-u", "autoprefixer-stylus", "-o", webFileAbs f, webFileAbs src])
+    [] 
+    sl 
+    fo
