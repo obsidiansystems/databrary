d9957854cfa0db08b7e0967b885885f5a7d58f4d
hsloan <ishaqsloan@gmail.com>
2017-08-18 22:08:26 -0400

    This script will quickly generate the databrary.conf script (rough draft)

---
+++ confgen.hs
@@ -0,0 +1,90 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [mtl text])" -i runhaskell
+
+-- This is a databrary.conf generator
+--TODO commented out components of example.conf must be added to
+--confContent
+--TODO File needs to be in ByteString
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import System.Process (callProcess)
+import System.Directory ( doesDirectoryExist
+                        , createDirectoryIfMissing
+                        , doesFileExist
+                        , getCurrentDirectory)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 (unlines)
+import qualified Data.ByteString.Char8 as ByteString8
+import Data.Monoid ((<>))
+
+main :: IO ()
+main = do
+  projectRoot <- getCurrentDirectory  
+  let databraryConf = "databrary.conf"
+  createFileIfMissing databraryConf $ confContent (ByteString8.pack projectRoot)
+
+confContent :: ByteString -> ByteString
+confContent absPath = ByteString8.unlines 
+  [ "##See example.conf for complete conf options."
+  , "secret = \"bob\""
+  , "port = 8000"
+  , "ssl {"
+  , "}"
+  , "log {"
+  , "  messages {"
+  , "    file = \"stderr\""
+  , "   rotate = 100"
+  , "  }"
+  , "  access {"
+  , "   file = \"stdout\""
+  , "   rotate = 100"
+  , "  }"
+  , "}"
+  , "db {"
+  , "  host = \"localhost\""
+  , "  port = 5432"
+  , "  user = \"databrary\""
+  , "  pass = \"databrary123\""
+  , "  db = \"databrary\""
+  , "}"
+  , ""
+  , "store {"
+  , "  master = \"" <> absPath <> "/store\""
+  , "  upload = \"" <> absPath <> "/upload\""
+  , "  temp  = \"" <> absPath <> "/tmp\""
+  , "  stage  = \"" <> absPath <> "/stage\""
+  , "  cache  = \"" <> absPath <> "/cache\""
+  , "  transcode {"
+  , "  }"
+  , "}"
+  , ""
+  , "solr {"
+  , "  run = false"
+  , "  host = \"localhost\""
+  , "  port = 8983"
+  , " home = \"" <> absPath <> "/solr\""
+  , "  core = \"databrary_core\""
+  , " log  = \"" <> absPath <> "/databrary_logs/solr_log\""
+  , "}"
+  , "static {"
+  , " authorize = \"bob@nyu.edu\""
+  , " assist = \"bob@nyu.edu\""
+  , "}"
+  , "ezid {"
+  , "}"
+  , "notification {"
+  , "  filter = \"*\""
+  , "  copy = \"bob@nyu.edu\""
+  , "}"
+  ]
+      
+createFileIfMissing :: FilePath -> ByteString -> IO ()
+createFileIfMissing aFile content =
+  doesFileExist aFile >>= \case
+    True -> putStrLn ("Skipping " ++ show aFile ++ "(already exist)")
+    False -> ByteString.writeFile aFile content
+  
