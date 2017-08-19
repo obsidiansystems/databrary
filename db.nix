17ffe2ea93bb1a2d9e8dfea3aef31c0b3307f0ae
Luigy Leon <luigy@outlook.com>
2017-08-19 19:38:07 -0400

    Move all db setup to db.nix

--- db.nix
+++ db.nix
@@ -1,10 +1,23 @@
 { reflex-platform ? import ./reflex-platform {}
 }:
 let
-	nixpkgs = reflex-platform.nixpkgs; 
+  nixpkgs = reflex-platform.nixpkgs;
   postgres = nixpkgs.postgresql96;
 in
 nixpkgs.writeScript "run-webdriver-tests" ''
-	${postgres}/bin/initdb -D databrary-db
-	${postgres}/bin/pg_ctl start -w -D databrary-db
+  ${postgres}/bin/initdb -D databrary-db
+  ${postgres}/bin/pg_ctl start -w -D databrary-db
+  ${postgres}/bin/createuser -s postgres
+  PGPASSWORD=mysecretpassword ${postgres}/bin/psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+      CREATE USER databrary;
+      CREATE DATABASE databrary;
+      GRANT ALL PRIVILEGES ON DATABASE databrary TO databrary;
+      ALTER USER databrary WITH PASSWORD 'databrary123';
+      ALTER USER databrary WITH SUPERUSER;
+  EOSQL
+
+  for file in ./schema/*
+  do
+    ${postgres}/bin/psql databrary < "$file"
+  done
 ''
