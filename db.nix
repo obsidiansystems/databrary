0c58313dc8fbc5a864da9c3f7416772a87143291
Luigy Leon <luigy@outlook.com>
2017-08-20 09:52:28 -0400

    Make db start script more robust to multiple runs.
    * Only create db if directory is missing
    * Only load schemas the first time the db is created.
    * Only start db if the status returned means it hasn't started.

--- db.nix
+++ db.nix
@@ -1,23 +1,34 @@
 { reflex-platform ? import ./reflex-platform {}
+, dbPath ? "databrary-db"
 }:
 let
   nixpkgs = reflex-platform.nixpkgs;
   postgres = nixpkgs.postgresql96;
 in
 nixpkgs.writeScript "run-webdriver-tests" ''
-  ${postgres}/bin/initdb -D databrary-db
-  ${postgres}/bin/pg_ctl start -w -D databrary-db
-  ${postgres}/bin/createuser -s postgres
-  PGPASSWORD=mysecretpassword ${postgres}/bin/psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-      CREATE USER databrary;
-      CREATE DATABASE databrary;
-      GRANT ALL PRIVILEGES ON DATABASE databrary TO databrary;
-      ALTER USER databrary WITH PASSWORD 'databrary123';
-      ALTER USER databrary WITH SUPERUSER;
-  EOSQL
+  if [ -d ${dbPath} ]; then
+     # Start the db if it's not running.
+     set +e
+     ${postgres}/bin/pg_ctl status -D "${dbPath}"
+     if [ $? -eq 3 ]; then
+       ${postgres}/bin/pg_ctl start -w -D "${dbPath}"
+     fi
+     set -e
+  else
+    ${postgres}/bin/initdb -D ${dbPath}
+    ${postgres}/bin/pg_ctl start -w -D ${dbPath}
+    ${postgres}/bin/createuser -s postgres
+    PGPASSWORD=mysecretpassword ${postgres}/bin/psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+        CREATE USER databrary;
+        CREATE DATABASE databrary;
+        GRANT ALL PRIVILEGES ON DATABASE databrary TO databrary;
+        ALTER USER databrary WITH PASSWORD 'databrary123';
+        ALTER USER databrary WITH SUPERUSER;
+EOSQL
 
-  for file in ./schema/*
-  do
-    ${postgres}/bin/psql databrary < "$file"
-  done
+    for file in ./schema/*
+    do
+      ${postgres}/bin/psql databrary < "$file"
+    done
+  fi
 ''
